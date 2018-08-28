3d92ee2ac46254f413b4dc3993931e0c92741776
varshakollu <varshakollu@gmail.com>
2018-08-28 09:53:40 -0500

    Revert "Branch1"

--- src/main/javascript/src/components/Unauthorized_page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export class Unauthorized_page extends React.Component {
-    render() {
-        return (
-            <div> <h3>You are not authorized to access this page </h3> </div>
-        );
-    }
-}
